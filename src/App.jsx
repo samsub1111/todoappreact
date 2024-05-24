122d4d2282788d424adbd80d6a325480f6f3a27d
“samsub1111” <premshiva2016@gmail.com>
2024-05-24 12:52:31 -0400

    Initialize TodoApp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import TodoList from "./TodoList";
+
+const App = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/todolist/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoApp = () => {
+  return (
+    <div>
+      <h2>Hello World </h2>
+    </div>
+  );
+};
+
+export default TodoApp;
